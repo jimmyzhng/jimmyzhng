0ea40a9591af8af6505090a1639d2848c993c2c2
Jimmy <jimmyzhang1@hotmail.com>
2023-02-25 20:37:17 -0800

    Add pacman loader for home and about pages

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -18,28 +19,32 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="container home-page">
+    <>
+      <div className="container home-page">
 
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
 
-          <br />
+            <br />
 
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoJ} alt="developer" />
-          <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15} />
-          <br />
-          <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={20} />
-        </h1>
-        <h2>Full-stack Web Developer | JavaScript Developer  </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-        <Link to="https://resume.creddle.io/resume/b0vfq90c1j7" target="_blank" className='flat-button'>RESUME</Link>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoJ} alt="developer" />
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={20} />
+          </h1>
+          <h2>Full-stack Web Developer | JavaScript Developer  </h2>
+          <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+          <Link to="https://resume.creddle.io/resume/b0vfq90c1j7" target="_blank" className='flat-button'>RESUME</Link>
 
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+
+    </>
   );
 }
