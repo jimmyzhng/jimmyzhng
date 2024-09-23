68937a627f2f481b701d4975a696b11286d641b7
Jimmy Zhang <jimmyzhang1@hotmail.com>
2024-09-23 11:12:15 -0700

    Update playlist to OSRS music only

--- src/data/playlist.js
+++ src/data/playlist.js
@@ -1,73 +1,131 @@
 const playlist = [
   {
-    name: 'Seven Lions @ Lake Kachess',
-    url: 'https://youtu.be/D7CaablOAY8&t=00s'
+    name: 'Dream',
+    url: 'https://www.youtube.com/watch?v=Np-lV0sl9Fw&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=73&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Mazare @ Cima Ekar Observatory',
-    url: 'https://www.youtube.com/watch?v=l3qPbDBmIxM&ab_channel=OpheliaRecords'
+    name: "Yesteryear",
+    url: "https://www.youtube.com/watch?v=FMNAcMNedOc&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&ab_channel=OldScapeMusic"
   },
   {
-    name: 'Sub Focus & Wilkinson @ Corfe Castle',
-    url: 'https://www.youtube.com/watch?v=TRh-amAhOEw&ab_channel=Mixmag'
+    name: 'Adventure',
+    url: 'https://www.youtube.com/watch?v=Z2Oci7962pI&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=108&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Gryffin (DJ Set) @ Kauai, Hawaii',
-    url: 'https://youtu.be/0fStWP79Z5A&t=00s'
+    name: 'Waterfall',
+    url: 'https://www.youtube.com/watch?v=D-rLFtiS1JA&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=7&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Anjunadeep 14 @ Qarraba Bay',
-    url: 'https://youtu.be/gaI7fdfyBHQ'
+    name: 'Harmony',
+    url: 'https://www.youtube.com/watch?v=JkR7zClEkAo&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Ben Bohmer @ Cappadocia, Turkey',
-    url: 'https://www.youtube.com/watch?v=RvRhUHTV_8k&ab_channel=Cercle'
+    name: 'Autumn Voyage',
+    url: 'https://www.youtube.com/watch?v=I4M4XTsx2cA&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Lane 8 @ Grand Lake, CO',
-    url: 'https://www.youtube.com/watch?v=n_LcVqqHSY8&ab_channel=ThisNeverHappened'
+    name: 'Still Night',
+    url: 'https://www.youtube.com/watch?v=XVjbGvHN-IY&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=20&ab_channel=OldScapeMusic'
   },
   {
-    name: 'John Summit @ Zamna Tulum',
-    url: 'https://youtu.be/wwqqWzlrL6M&t=00s'
+    name: 'Spirit',
+    url: 'https://www.youtube.com/watch?v=cZPE-45HJIQ&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=23&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Anjunadeep 11 - Continuous Mix',
-    url: 'https://www.youtube.com/watch?v=-9BM6jGxW5Y&ab_channel=Anjunadeep'
+    name: 'Newbie Melody',
+    url: 'https://www.youtube.com/watch?v=VNW9TAwImBY&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=39&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Anjunadeep 13 @ Lake Skadar',
-    url: 'https://www.youtube.com/watch?v=QM7L27N8-rE&ab_channel=Anjunadeep'
+    name: 'Medieval',
+    url: 'https://www.youtube.com/watch?v=QSkvyG3H_KM&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=44&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Delta Heavy @ Rampage 2022',
-    url: 'https://www.youtube.com/watch?v=ghOdbnsFwFQ&ab_channel=WeAreRampageEvents'
+    name: 'Garden',
+    url: 'https://www.youtube.com/watch?v=WtBigNNwcmo&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=64&ab_channel=OldScapeMusic'
   },
   {
-    name: "ZHU @ Hakuba Iwatake (Japan)",
-    url: 'https://www.youtube.com/watch?v=XHNHq1mC0VQ&ab_channel=Cercle'
+    name: 'Flute Salad',
+    url: 'https://www.youtube.com/watch?v=LFgvAyJWkjU&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=66&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Ben Bohmer @ Anjunadeep Prague',
-    url: 'https://www.youtube.com/watch?v=b9kThoLdDc8&ab_channel=Anjunadeep'
+    name: 'The Trade Parade',
+    url: 'https://www.youtube.com/watch?v=LS6PfoPIgns&ab_channel=OldScapeMusic'
   },
   {
-    name: 'Tchami - Elevation Sunrise Set',
-    url: 'https://www.youtube.com/watch?v=CQBV6cxeI40&t=24s&ab_channel=Tchami'
-  },
+    name: 'Fanfare',
+    url: 'https://www.youtube.com/watch?v=k0wd7pAVR1M&list=PLA3CCrYGqFawo3BuPXD1ZHgINFGV76Vu3&index=68&ab_channel=OldScapeMusic'
+  }
 
-  {
-    name: 'John Summit - BBC Radio Essential Mix',
-    url: 'https://www.youtube.com/watch?v=S0Q1GElnrwY&ab_channel=JohnSummit'
-  },
-  {
-    name: 'Fred again.. @ Boiler Room',
-    url: 'https://www.youtube.com/watch?v=c0-hvjV2A5Y&ab_channel=BoilerRoom'
-  },
-  {
-    name: 'SpongeBob Relaxing Music',
-    url: 'https://www.youtube.com/watch?v=XWsptjpzBW0&ab_channel=giamoforti'
-  },
+
+  // {
+  //   name: 'Seven Lions @ Lake Kachess',
+  //   url: 'https://youtu.be/D7CaablOAY8&t=00s'
+  // },
+  // {
+  //   name: 'Mazare @ Cima Ekar Observatory',
+  //   url: 'https://www.youtube.com/watch?v=l3qPbDBmIxM&ab_channel=OpheliaRecords'
+  // },
+  // {
+  //   name: 'Sub Focus & Wilkinson @ Corfe Castle',
+  //   url: 'https://www.youtube.com/watch?v=TRh-amAhOEw&ab_channel=Mixmag'
+  // },
+  // {
+  //   name: 'Gryffin (DJ Set) @ Kauai, Hawaii',
+  //   url: 'https://youtu.be/0fStWP79Z5A&t=00s'
+  // },
+  // {
+  //   name: 'Anjunadeep 14 @ Qarraba Bay',
+  //   url: 'https://youtu.be/gaI7fdfyBHQ'
+  // },
+  // {
+  //   name: 'Ben Bohmer @ Cappadocia, Turkey',
+  //   url: 'https://www.youtube.com/watch?v=RvRhUHTV_8k&ab_channel=Cercle'
+  // },
+  // {
+  //   name: 'Lane 8 @ Grand Lake, CO',
+  //   url: 'https://www.youtube.com/watch?v=n_LcVqqHSY8&ab_channel=ThisNeverHappened'
+  // },
+  // {
+  //   name: 'John Summit @ Zamna Tulum',
+  //   url: 'https://youtu.be/wwqqWzlrL6M&t=00s'
+  // },
+  // {
+  //   name: 'Anjunadeep 11 - Continuous Mix',
+  //   url: 'https://www.youtube.com/watch?v=-9BM6jGxW5Y&ab_channel=Anjunadeep'
+  // },
+  // {
+  //   name: 'Anjunadeep 13 @ Lake Skadar',
+  //   url: 'https://www.youtube.com/watch?v=QM7L27N8-rE&ab_channel=Anjunadeep'
+  // },
+  // {
+  //   name: 'Delta Heavy @ Rampage 2022',
+  //   url: 'https://www.youtube.com/watch?v=ghOdbnsFwFQ&ab_channel=WeAreRampageEvents'
+  // },
+  // {
+  //   name: "ZHU @ Hakuba Iwatake (Japan)",
+  //   url: 'https://www.youtube.com/watch?v=XHNHq1mC0VQ&ab_channel=Cercle'
+  // },
+  // {
+  //   name: 'Ben Bohmer @ Anjunadeep Prague',
+  //   url: 'https://www.youtube.com/watch?v=b9kThoLdDc8&ab_channel=Anjunadeep'
+  // },
+  // {
+  //   name: 'Tchami - Elevation Sunrise Set',
+  //   url: 'https://www.youtube.com/watch?v=CQBV6cxeI40&t=24s&ab_channel=Tchami'
+  // },
+
+  // {
+  //   name: 'John Summit - BBC Radio Essential Mix',
+  //   url: 'https://www.youtube.com/watch?v=S0Q1GElnrwY&ab_channel=JohnSummit'
+  // },
+  // {
+  //   name: 'Fred again.. @ Boiler Room',
+  //   url: 'https://www.youtube.com/watch?v=c0-hvjV2A5Y&ab_channel=BoilerRoom'
+  // },
+  // {
+  //   name: 'SpongeBob Relaxing Music',
+  //   url: 'https://www.youtube.com/watch?v=XWsptjpzBW0&ab_channel=giamoforti'
+  // },
 ];
 
 export default playlist;
